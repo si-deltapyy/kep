852622bc81b6fa6a0839f872dc7bbd4012bfa1ed
Humanioraaa <muhhilmynaufal03@gmail.com>
2024-12-20 13:46:58 +0700

    fix ec document view

--- resources/views/pages/ec/create.blade.php
+++ resources/views/pages/ec/create.blade.php
@@ -1,16 +1,57 @@
-<form action="{{ route('sekertaris.ec.store') }}" method="POST" has-file enctype="multipart/form-data">
-    @csrf
 
-    <input id="title" name="id" type="text" value="{{ $data->id}}" hidden>
-    <input id="title" name="user" type="text" value="{{ $user->id }}" hidden>
-    <label for="title">Judul Ajuan</label>
-    <input id="title" name="title" type="text" value="{{ $data->title }}" readonly muted>
 
-    <label for="name">Name</label>
-    <input id="name" name="name" type="text" value="{{ $user->name }}" readonly muted>
 
-    <label for="file">Upload EC</label>
-    <input id="file" name="file" type="file" accept=".pdf">
+@extends('layouts.app')
+@section('title')
+<x-page-tittle :title="'EC Document'" :slash1="'EC Document'" :slash2="'Dokumen'" :slash3="'Create'"></x-page-tittle>
+@endsection
 
-    <button type="submit">Upload</button>
-</form>
+
+
+@section('content')
+        <div
+            class="grid md:grid-cols-12 lg:grid-cols-12 xl:grid-cols-12 gap-4 mb-4"
+          >
+            <div
+              class="sm:col-span-12 md:col-span-12 lg:col-span-8 xl:col-span-6 xl:col-start-4"
+            >
+              <div
+                class="bg-white dark:bg-gray-900 border border-slate-200 dark:border-slate-700/40 rounded-md w-full relative mb-4"
+              >
+                <div
+                  class="border-b border-slate-200 dark:border-slate-700/40 py-3 px-4 dark:text-slate-300/70"
+                >
+                  <div class="flex-none md:flex">
+                    <h4
+                      class="font-medium text-lg flex-1 self-center mb-2 md:mb-0"
+                    >
+                      Pengajuan Dokumen
+                    </h4>
+                  </div>
+                </div>
+                <!--end header-title-->
+                <div class="flex-auto p-4">
+                  <x-form-input action="{{ route('sekertaris.ec.store') }}" method="POST" has-file enctype="multipart/form-data">
+                    <input id="title" name="id" type="text" value="{{ $data->id}}" hidden>
+                    <input id="title" name="user" type="text" value="{{ $user->id }}" hidden>
+
+
+
+
+                    <x-input title="Nama Pengusul " value="{{ $user->name }}" id="name" name="name" type="text" class="form-control" readonly muted/>
+                    <x-input title="Judul Usulan " id="title" name="title" type="text" value="{{ $data->title }}" class="form-control" readonly muted/>
+
+                    <div class="col-span-1">
+                        <x-file-upload title="Upload File" id="file" name="file" type="file" accept=".pdf" class="file-input"/><br>
+                    </div>
+
+                    <x-button>Submit</x-button>
+                  </x-form-input>
+                </div>
+                <!--end card-body-->
+              </div>
+              <!--end card-->
+            </div>
+            <!--end col-->
+          </div>
+@endsection
