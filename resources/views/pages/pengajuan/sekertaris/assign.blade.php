aa383adec66163d4b5bcec2147e0ea8e0448713a
Humanioraaa <muhhilmynaufal03@gmail.com>
2024-12-20 13:18:39 +0700

    try to fix multi select

--- resources/views/pages/pengajuan/sekertaris/assign.blade.php
+++ resources/views/pages/pengajuan/sekertaris/assign.blade.php
@@ -4,6 +4,7 @@
 @endsection
 
 @section('content')
+
 <div
             class="grid md:grid-cols-12 lg:grid-cols-12 xl:grid-cols-12 gap-4 mb-4"
           >
@@ -30,9 +31,6 @@
                   <x-form-input action="{{route('sekertaris.pengajuan.update', $dummy->id)}}" method="post">
                     @csrf
                     @method('PUT')
-                    {{-- <x-input title="Nama" id="pengusul" value="" type="text" class="form-control" name="name"/> --}}
-                    {{-- <x-input title="Email" id="pengusul" value="d" type="text" class="form-control" name="email"/> --}}
-
                     <div class="w-full mb-3">
                         <label for="review" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                             Pilih Reviewer
@@ -48,10 +46,9 @@
                                 <option value="" disabled>Reviewer tidak tersedia</option>
                             @endif
                         </select>
+
+
                     </div>
-
-
-
                     <button
                       type="submit"
                       class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500 text-sm font-medium py-1 px-3 rounded mb-1 lg:mb-0"
@@ -72,65 +69,6 @@
             </div>
             <!--end col-->
         </div>
-@endsection
-
-{{-- <table border="1">
-    <thead>
-        <tr>
-            <td>No</td>
-            <td>Judul Dokumen</td>
-            <td>Status</td>
-            @foreach($dummy as $x)
-            @if($x->doc_status == 'new-proposal')
-            <td>Action</td>
-            <td>Reviewer</td>
-            @endif
-            @endforeach
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($dummy as $x)
-        <tr>
-            <td>{{$x->id}}</td>
-            <td>{{$x->title}}<br>
-                @foreach($doc as $xp)
-                    - {{$xp->doc_name}}
-                    @if($x->doc_status == 'new-proposal')
-                    <a href="/storage/{{$x->doc_path}}" target="_blank" rel="noopener noreferrer">**</a>
-                    @endif
-                    <br>
-                @endforeach
-            </td>
-            <td>{{$x->doc_status}}</td>
-            @if($x->doc_status == 'new-proposal')
-            <td>
-            <form action="{{route('sekretariat.pengajuan.store', $x->doc_group)}}" method="post">
-                    @csrf
-                    <select name="review[]" multiple>
-                        <option value="0">-- Pilih Reviewer --</option>
-                        @foreach($reviewer as $xa)
-                        <option value="{{$xa->id}}">{{$xa->name}}</option>
-                        @endforeach
-                    </select>
-            </td>
-            <td>
-                <button type="submit">assign reviewer</button>
-            </form>
-            </td>
-            @endif
-        </tr>
-        @endforeach
-    </tbody>
-</table> --}}
-
-
-
-
-
-
-
-
-
-
+        @endsection
 
 
