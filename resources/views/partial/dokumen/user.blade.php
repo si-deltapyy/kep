1056ca2a7a9ae6948cd0adf9f9e116d971d58541
Yusfia Hafid Aristyagama <yusfia.hafid@staff.uns.ac.id>
2025-05-07 10:02:19 +0700

    > file upload form edit > user template download update

--- resources/views/partial/dokumen/user.blade.php
+++ resources/views/partial/dokumen/user.blade.php
@@ -1,5 +1,7 @@
 @if (session('success'))
-    <div id="errorAlert" class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded-md shadow-lg mb-4 relative" role="alert">
+    <div id="errorAlert"
+         class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded-md shadow-lg mb-4 relative"
+         role="alert">
         <div class="flex items-center">
             <div class="mr-3">
                 <span data-lucide="badge-check" class="w-6 h-6 text-green-500"></span>
@@ -9,7 +11,8 @@
                 <p>{{ session('success') }}</p>
             </div>
         </div>
-        <button type="button" onclick="closeAlert('errorAlert')" class="absolute top-2 right-2 text-green-500 hover:text-green-700 focus:outline-none">
+        <button type="button" onclick="closeAlert('errorAlert')"
+                class="absolute top-2 right-2 text-green-500 hover:text-green-700 focus:outline-none">
             <span data-lucide="x" class="w-5 h-5"></span>
         </button>
     </div>
@@ -26,19 +29,19 @@
 
 @if(session('error'))
     <div class="p-3 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
-        <span class="font-medium">alert!</span>  {{ session('error') }}
+        <span class="font-medium">alert!</span> {{ session('error') }}
     </div>
 @endif
 
 <div class="flex-auto p-6 text-left">
     <div class="flex justify-left gap-4 mt-4">
         <a href="javascript:void(0);" onclick="showModal()"
-            class="px-2 py-1 bg-primary-500/10 border border-transparent text-primary text-sm rounded hover:bg-blue-600 hover:text-white">
+           class="px-2 py-1 bg-primary-500/10 border border-transparent text-primary text-sm rounded hover:bg-blue-600 hover:text-white">
             <i class="ti ti-plus me-1"></i> Ajukan
             <span data-lucide="plus" class="w-4 h-4 inline-block me-2"></span>
         </a>
         <button type="button" data-fc-type="modal" data-fc-target="modalcenter"
-            class="inline-block focus:outline-none bg-green-500/10 text-green hover:bg-green-500 hover:text-white border border-gray-200 dark:bg-transparent dark:text-slate-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-slate-500 text-sm font-medium py-1 px-3 rounded">
+                class="inline-block focus:outline-none bg-green-500/10 text-green hover:bg-green-500 hover:text-white border border-gray-200 dark:bg-transparent dark:text-slate-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-slate-500 text-sm font-medium py-1 px-3 rounded">
             <i data-lucide="download" class="w-4 h-4 inline-block me-2"></i> Download Template
         </button>
     </div>
@@ -73,7 +76,8 @@
             <h2 class="text-lg font-semibold">Perlu Diperhatikan</h2>
         </div>
         <div class="p-4">
-            <p class="text-gray-700">Silakan untuk mengunduh <strong>Template Pengajuan</strong> sesuai Dengan Bidang.</p>
+            <p class="text-gray-700">Silakan untuk mengunduh <strong>Template Pengajuan</strong> sesuai Dengan Bidang.
+            </p>
         </div>
         <div class="flex justify-end space-x-4 px-4 py-2">
             <!-- Tombol Ajukan -->
@@ -96,35 +100,55 @@
 
 {{-- Template Modal --}}
 <div class="modal animate-ModalSlide hidden" id="modalcenter">
-    <div class="relative w-auto pointer-events-none sm:max-w-lg sm:my-0 sm:mx-auto z-[99] flex items-center h-[calc(100%-3.5rem)]">
-        <div class="relative flex flex-col w-full pointer-events-auto bg-white dark:bg-slate-800 bg-clip-padding rounded">
-            <div class="flex shrink-0 items-center justify-between py-2 px-4 rounded-t border-b border-solid dark:border-gray-700 bg-[#603dc3] dark:bg-[#603dc3] dark:text-white text-white">
-                <h6 class="mb-0 leading-4 text-base font-semibold text-white mt-0" id="staticBackdropLabel1">Unduh Template</h6>
-                <button type="button" class="box-content w-4 h-4 p-1 bg-green-700/60 rounded-full text-slate-300 leading-4 text-xl close" aria-label="Close" data-fc-dismiss>&times;</button>
+    <div
+        class="relative w-auto pointer-events-none sm:max-w-lg sm:my-0 sm:mx-auto z-[99] flex items-center h-[calc(100%-3.5rem)]">
+        <div
+            class="relative flex flex-col w-full pointer-events-auto bg-white dark:bg-slate-800 bg-clip-padding rounded">
+            <div
+                class="flex shrink-0 items-center justify-between py-2 px-4 rounded-t border-b border-solid dark:border-gray-700 bg-[#603dc3] dark:bg-[#603dc3] dark:text-white text-white">
+                <h6 class="mb-0 leading-4 text-base font-semibold text-white mt-0" id="staticBackdropLabel1">Unduh
+                    Template</h6>
+                <button type="button"
+                        class="box-content w-4 h-4 p-1 bg-green-700/60 rounded-full text-slate-300 leading-4 text-xl close"
+                        aria-label="Close" data-fc-dismiss>&times;
+                </button>
             </div>
             <div data-fc-type="accordion">
                 @foreach ($types as $type)
-                <h2 id="accordion-collapse-heading-1" class="bg-white"  data-fc-type="collapse">
-                    <button type="button" class="fc-collapse-open:text-primary flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200  dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-                      <span>Template {{ $type->ajuan_name }}</span>
-                      <i class="fas fa-angle-down  fc-collapse-open:rotate-180 transition-transform duration-300" data-accordion-icon></i>
-                    </button>
-                  </h2>
-                  <div id="accordion-collapse-body-1" class="hidden overflow-hidden transition-[height] duration-300" aria-labelledby="accordion-collapse-heading-1">
-                    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                      <p class="mb-2 text-gray-500 dark:text-gray-400">Unduh Template {{ $type->ajuan_name }}</p>
-                        @foreach($type->template as $template)
-                            {{$loop->index + 1}}. {{$template->template_name}}
-                            <a href="{{ asset('/app/' . $template->template_path) }}" class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded mr-1 close" data-fc-dismiss>Unduh</a>
-                        @endforeach
-{{--                      <a href="{{ route('user.download.templates', $type->id) }}" class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded mr-1 close" data-fc-dismiss>Unduh</a>--}}
+                    <h2 id="accordion-collapse-heading-1" class="bg-white" data-fc-type="collapse">
+                        <button type="button"
+                                class="fc-collapse-open:text-primary flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200  dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                                data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
+                                aria-controls="accordion-collapse-body-1">
+                            <span>Template {{ $type->ajuan_name }}</span>
+                            <i class="fas fa-angle-down  fc-collapse-open:rotate-180 transition-transform duration-300"
+                               data-accordion-icon></i>
+                        </button>
+                    </h2>
+                    <div id="accordion-collapse-body-1" class="hidden overflow-hidden transition-[height] duration-300"
+                         aria-labelledby="accordion-collapse-heading-1">
+                        <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">Unduh Template {{ $type->ajuan_name }}</p>
+                            <ul>
+                                @foreach($type->template as $template)
+                                    <li>
+                                        {{$loop->index + 1}}. {{$template->template_name}}
+                                        <a href="{{ asset('/app/' . $template->template_path) }}"
+                                           class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded mr-1 close"
+                                           data-fc-dismiss>Unduh</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
-                  </div>
                 @endforeach
 
             </div>
             <div class="flex flex-wrap shrink-0 justify-end p-3  rounded-b border-t border-dashed dark:border-gray-700">
-                <button class="inline-block focus:outline-none text-red-500 hover:bg-red-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-red-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-red-500  text-sm font-medium py-1 px-3 rounded mr-1 close" data-fc-dismiss>Close</button>
+                <button
+                    class="inline-block focus:outline-none text-red-500 hover:bg-red-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-red-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-red-500  text-sm font-medium py-1 px-3 rounded mr-1 close"
+                    data-fc-dismiss>Close
+                </button>
             </div>
         </div>
     </div>
